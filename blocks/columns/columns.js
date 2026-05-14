e2bf73ec578cbaf36d173774486f4c77645c483a
Ankit-Perficient <ankit.pardhi@perficient.com>
2026-05-14 18:51:26 +0530

    Revert "columns chages added"
    This reverts commit 3ef80975c8e38e50edab73a3d472346d2918994d.

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,5 +1,3 @@
-import { decorateBlock, loadBlock } from '../../scripts/aem.js';
-
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
@@ -16,11 +14,5 @@ export default function decorate(block) {
         }
       }
     });
-  });
-
-  // decorate and load any nested blocks inside columns
-  block.querySelectorAll(':scope > div > div > div').forEach((nestedBlock) => {
-    decorateBlock(nestedBlock);
-    loadBlock(nestedBlock);
   });
 }
