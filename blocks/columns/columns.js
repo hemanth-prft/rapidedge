11db396349db09dd023d69648964c9bac058a38a
Ankit-Perficient <ankit.pardhi@perficient.com>
2026-05-15 13:34:45 +0530

    Column Fix

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,18 +1,25 @@
+import { decorateBlock, loadBlock } from "../../scripts/aem";
+
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
   // setup image columns
-  [...block.children].forEach((row) => {
-    [...row.children].forEach((col) => {
-      const pic = col.querySelector('picture');
-      if (pic) {
-        const picWrapper = pic.closest('div');
-        if (picWrapper && picWrapper.children.length === 1) {
-          // picture is only content in column
-          picWrapper.classList.add('columns-img-col');
-        }
-      }
-    });
-  });
+  // [...block.children].forEach((row) => {
+  //   [...row.children].forEach((col) => {
+  //     const pic = col.querySelector('picture');
+  //     if (pic) {
+  //       const picWrapper = pic.closest('div');
+  //       if (picWrapper && picWrapper.children.length === 1) {
+  //         // picture is only content in column
+  //         picWrapper.classList.add('columns-img-col');
+  //       }
+  //     }
+  //   });
+  // });
+  const children = [...block.querySelectorAll('.block')];
+  children.forEach((row) => decorateBlock(row));
+  await Promise.all(
+    children.map((row) => loadBlock(row))
+  );
 }
