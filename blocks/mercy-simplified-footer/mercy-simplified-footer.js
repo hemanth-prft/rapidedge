b01d06847e66742a060f449e9563a41539c49818
Vasanth Thangadurai <vasanth.thangadurai@perficient.com>
2026-07-08 12:51:49 +0530

    fixed lint issues

--- blocks/mercy-simplified-footer/mercy-simplified-footer.js
+++ blocks/mercy-simplified-footer/mercy-simplified-footer.js
@@ -2,30 +2,30 @@ export default function decorate(block) {
   const currentYear = new Date().getFullYear();
 
   block.innerHTML = `
-    <div class="mercy-simplified-footer__content">
-      <div class="mercy-simplified-footer__section">
-        <a class="mercy-simplified-footer__logo" href="/" aria-label="Mercy Home">
+    <div class="mercy-simplified-footer-content">
+      <div class="mercy-simplified-footer-section">
+        <a class="mercy-simplified-footer-logo" href="/" aria-label="Mercy Home">
           <img src="/blocks/footer/reversedLogo.png" alt="Mercy" />
         </a>
       </div>
-      <div class="mercy-simplified-footer__links">
-        <ul class="mercy-simplified-footer__list">
-          <li class="mercy-simplified-footer__item">
-            <span class="mercy-simplified-footer__copyright">Mercy, St. Louis</span>
+      <div class="mercy-simplified-footer-links">
+        <ul class="mercy-simplified-footer-list">
+          <li class="mercy-simplified-footer-item">
+            <span class="mercy-simplified-footer-copyright">Mercy, St. Louis</span>
           </li>
-          <li class="mercy-simplified-footer__item">
-            <span class="mercy-simplified-footer__copyright">615 South New Ballas Road</span>
+          <li class="mercy-simplified-footer-item">
+            <span class="mercy-simplified-footer-copyright">615 South New Ballas Road</span>
           </li>
-          <li class="mercy-simplified-footer__item">
-            <span class="mercy-simplified-footer__copyright">Saint Louis, Missouri 63141</span>
+          <li class="mercy-simplified-footer-item">
+            <span class="mercy-simplified-footer-copyright">Saint Louis, Missouri 63141</span>
           </li>
         </ul>
-        <ul class="mercy-simplified-footer__list">
-          <li class="mercy-simplified-footer__item">
-            <a class="mercy-simplified-footer__link" href="https://www.mercy.net/about/legal-notices/">Terms &amp; Privacy</a>
+        <ul class="mercy-simplified-footer-list">
+          <li class="mercy-simplified-footer-item">
+            <a class="mercy-simplified-footer-link" href="https://www.mercy.net/about/legal-notices/">Terms &amp; Privacy</a>
           </li>
-          <li class="mercy-simplified-footer__item">
-            <p class="mercy-simplified-footer__copyright">&copy; ${currentYear} Mercy</p>
+          <li class="mercy-simplified-footer-item">
+            <p class="mercy-simplified-footer-copyright">&copy; ${currentYear} Mercy</p>
           </li>
         </ul>
       </div>
